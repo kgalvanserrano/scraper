e1c5f6c2cf8679f614c5193287ebf04ab79106c9
kgalvanserrano <kevin.galvan@live.com>
2025-08-21 11:20:03 -0700

    Implement page fetching and parsing with BeautifulSoup

--- scraper.py
+++ scraper.py
@@ -1,3 +1,10 @@
 import requests
 from bs4 import BeautifulSoup
 
+# fetch and parse the page
+response = requests.get('https://kgalvan.dev')
+soup = BeautifulSoup(response.text, 'html.parser') # converts html into searchable object, html.parser is the built-in parser
+print(soup.prettify()) # formats the html nicely for easier reading
+
+# find main content container
+content_div = soup.find('div', class_='main-content')
